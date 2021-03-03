6808f2d8dfd549b710a300714eda41857153830b
breno23augusto <breno23augusto@hotmail.com>
2021-03-02 22:52:11 -0300

    updating css

--- src/public/index.php
+++ src/public/index.php
@@ -8,27 +8,27 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="col text-center text-light">
-            <h1><?php echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?></h1>
+
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col ">
+                <pre>
+                    <code class="language-json">
+                        {
+                            "current-page": "home",
+                            "description": "Hi! I'm a backend developer, and I really want to study and learn more about everything!",
+                            "author": "Breno Augusto Ribeiro Campos", 
+                            "occupation": "Backend Developer"
+                            "links": {
+                                "linkedin": "https://www.linkedin.com/in/breno-augusto-ribeiro-campos-787863161",
+                                "github": "https://github.com/breno23augusto"
+                            }
+                        }
+                    </code>
+                </pre>
+            </div>
         </div>
     </div>
-
-    <pre>
-        <code class="language-json">
-            {
-                "current-page": "home",
-                "description": "Hi! I'm a backend developer, and I really want to study and learn more about everything!",
-                "author": "Breno Augusto Ribeiro Campos", 
-                "occupation": "Backend Developer"
-                "links": {
-                    "linkedin": "https://www.linkedin.com/in/breno-augusto-ribeiro-campos-787863161",
-                    "github": "https://github.com/breno23augusto"
-                }
-            }
-        </code>
-    </pre>
-
     <script src="resources/js/prism.js"></script>
 </body>
 
